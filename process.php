6483dac374ea516031c897ec05393cf52e59c9dc
John Romell <jromell099@gmail.com>
2024-09-25 21:53:11 +0800

    Updated Bug where email body does not allow whitespaces

--- process.php
+++ process.php
@@ -7,16 +7,24 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     $garbageType = $_POST["garbagetype"];
 
     $to = 'grabmygarbageproj@gmail.com';
-    $header = "From: $name";
-    $subject = 'Grab my Garbage Request!';
-    $message = <<<EOT
-                    Name: $name
-                    Address: $address
-                    Phone Number: $phone
-                    Garbage Type: $garbageType
-                EOT;
+    $subject = "Grab my Garbage Request!";
+    $messageLines = array(
+        "Name: $name",
+        "Address: $address",
+        "Phone Number: $phone",
+        "Garbage Type: $garbageType"
+    );
+    $message = implode("\n", $messageLines);
 
-    if(mail($to,$subject,$message,$header)){
+    $headers = array(
+        "From: grabmygarbageproj@gmail.com",
+        "Reply-To: grabmygarbageproj@gmail.com",
+        "MIME-Version: 1.0",
+        "Content-Type: text/plain; charset=UTF-8"
+    );
+    $headersString = implode("\r\n", $headers);
+
+    if(mail($to, $subject, $message, $headersString)){
         echo 'Email sent!';
     } else {
         echo 'Email sending failed!';
